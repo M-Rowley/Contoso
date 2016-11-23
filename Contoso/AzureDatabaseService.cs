9d2b946b4892c7cc5a513a555bf751ca9221dbfb
Mike <mrow277@aucklanduni.ac.nz>
2016-11-24 02:12:42 +1300

    CRUD backend done, need to add functionality to message controller

---
+++ Contoso/AzureDatabaseService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Contoso.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Contoso
+{
+    public class AzureDatabaseService
+    {
+        //Initialise instance and client
+        public static AzureDatabaseService instance;
+        public MobileServiceClient client;
+        public IMobileServiceTable<ContosoBankAccounts> bankAccountTable;
+
+        //explicit constructor
+        AzureDatabaseService()
+        {
+            this.client = new MobileServiceClient("http://MrowContosoMobile.azurewebsites.net");
+            this.bankAccountTable = this.client.GetTable<ContosoBankAccounts>();
+        }
+
+        //POST (C)
+        public async Task addAccount(ContosoBankAccounts bankAccount)
+        {
+            await this.bankAccountTable.InsertAsync(bankAccount);
+        }
+
+        //GET (R)
+        public async Task<List<ContosoBankAccounts>> getAccounts()
+        {
+            return await this.bankAccountTable.ToListAsync();
+        }
+
+        //PUT (U)
+        public async Task updateAccount(ContosoBankAccounts bankAccount)
+        {
+            await this.bankAccountTable.UpdateAsync(bankAccount);
+        }
+
+        //DELETE (D)
+        public async Task deleteAccount(ContosoBankAccounts bankAccount)
+        {
+            await this.bankAccountTable.DeleteAsync(bankAccount);
+        }
+    }
+}
---
+++ Contoso/DataModels/ContosoBankAccounts.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contoso.DataModels
+{
+    public class ContosoBankAccounts
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+        [JsonProperty(PropertyName = "createdAt")]
+        public DateTime createdAt { get; set; }
+
+        [JsonProperty(PropertyName = "updatedAt")]
+        public DateTime updatedAt { get; set; }
+
+        [JsonProperty(PropertyName = "deleted")]
+        public bool deleted { get; set; }
+
+        [JsonProperty(PropertyName = "balance")]
+        public double balance { get; set; }
+    }
+}
